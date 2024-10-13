4e21c6371af057b055dfc1eacd1c163c132aefec
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-10-12 21:12:17 -0300

    Finalized the invoice template, along with the modal to create an invoice

--- src/app/pages/invoice/invoice.component.ts
+++ src/app/pages/invoice/invoice.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../components/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, CommonModule],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
+
+    isModalVisible: boolean = false;
+
+    openModal() {
+        this.isModalVisible = true;
+    }
+
+    closeModal() {
+        this.isModalVisible = false;
+    }
 
 }
