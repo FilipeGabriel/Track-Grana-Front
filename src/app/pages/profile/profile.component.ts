f77c9149d6fc4ca51575b3cb29030fe4af8cee1b
Filipe Gabriel de Lira Moura <filipe@direction.com.br>
2024-10-15 12:24:43 -0300

    Function for importing a local image finished

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TitleComponent } from '../../components/title/title.component';
 
 @Component({
@@ -9,5 +9,24 @@ import { TitleComponent } from '../../components/title/title.component';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+    selectedFile: File | null = null;
+    imageSrc: string | ArrayBuffer | null = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwGsNv23K5shKblMsKePA8o6M2kqBH39PZqA&s" // Para armazenar a URL da imagem
+    @ViewChild('fileInput') fileInput!: ElementRef;
+
+    onFileSelected(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files.length > 0) {
+        this.selectedFile = input.files[0];
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            if (e.target?.result) {
+            this.imageSrc = e.target.result; // Armazena a imagem como URL
+            }
+        };
+        reader.readAsDataURL(this.selectedFile); // Lê o arquivo como URL de dados
+        }
+    }
 
 }
