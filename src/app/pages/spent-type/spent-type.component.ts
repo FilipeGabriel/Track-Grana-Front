4155997d1635176d7d278fcff9f9d1ba73a7c61d
Filipe Gabriel de Lira Moura <filipe@direction.com.br>
2024-10-17 13:21:31 -0300

    Opening the modal through the card

--- src/app/pages/spent-type/spent-type.component.ts
+++ src/app/pages/spent-type/spent-type.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../components/title/title.component';
 import { SpentCardComponent } from '../../components/spent-card/spent-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-spent-type',
   standalone: true,
-  imports: [TitleComponent, SpentCardComponent],
+  imports: [TitleComponent, SpentCardComponent, CommonModule],
   templateUrl: './spent-type.component.html',
   styleUrl: './spent-type.component.css'
 })
 export class SpentTypeComponent {
+
+    isModalVisible: boolean = false;
+
+    openModal() {
+        this.isModalVisible = true;
+    }
+
+    closeModal() {
+        this.isModalVisible = false;
+    }
 
 }
