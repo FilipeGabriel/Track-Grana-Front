50ea7178f50aeb7757dfe19feb289e97fbbea908
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-10-16 22:11:23 -0300

    Created card component to use as a spent type item

--- src/app/pages/spent-type/spent-type.component.ts
+++ src/app/pages/spent-type/spent-type.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../components/title/title.component';
+import { SpentCardComponent } from '../../components/spent-card/spent-card.component';
 
 @Component({
   selector: 'app-spent-type',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, SpentCardComponent],
   templateUrl: './spent-type.component.html',
   styleUrl: './spent-type.component.css'
 })
