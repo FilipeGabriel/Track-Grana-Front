2e7423c2ef6ca690fa147df018f42e13086a6a43
Filipe Gabriel de Lira Moura <filipe@direction.com.br>
2024-10-25 11:47:36 -0300

    Created the Account class, partial finalization of the business rules on the user profile screen

---
+++ src/app/models/account.ts
@@ -0,0 +1,10 @@
+export class Account {
+
+    name: string;
+    cpf: string;
+    telephone: string;
+    birthDate: string;
+    accountImg: string | ArrayBuffer | null;
+    userId: number;
+
+}
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,19 +1,42 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TitleComponent } from '../../components/title/title.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Account } from '../../models/account';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [TitleComponent, CommonModule],
+  imports: [TitleComponent, CommonModule, FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+    account: Account;
+    trueAccount: Account;
 
     selectedFile: File | null = null;
     imageSrc: string | ArrayBuffer | null = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwGsNv23K5shKblMsKePA8o6M2kqBH39PZqA&s" // Para armazenar a URL da imagem
     isModalVisible: boolean = false;
+
+    constructor() {
+        this.account = new Account();
+        this.trueAccount = new Account();
+    }
+
+    ngOnInit() {
+        this.account = {
+            name: 'João',
+            cpf: '11201955432',
+            telephone: '81984946724',
+            birthDate: '18/04/1997',
+            accountImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwGsNv23K5shKblMsKePA8o6M2kqBH39PZqA&s',
+            userId: 1
+        };
+
+        this.trueAccount = { ...this.account };
+    }
 
 
     @ViewChild('fileInput') fileInput!: ElementRef;
@@ -26,11 +49,20 @@ export class ProfileComponent {
         const reader = new FileReader();
         reader.onload = (e) => {
             if (e.target?.result) {
-            this.imageSrc = e.target.result; // Armazena a imagem como URL
+            this.account.accountImg = e.target.result;
             }
         };
-        reader.readAsDataURL(this.selectedFile); // Lê o arquivo como URL de dados
+        reader.readAsDataURL(this.selectedFile);
         }
+    }
+
+    onSubmit() {
+        this.trueAccount = { ...this.account }
+        console.log(this.account);
+    }
+
+    cancelAlteration() {
+        this.account = { ...this.trueAccount }
     }
 
     openModal() {
