3d911bd3310a1a64d803649decdc612a0ffbe72c
Filipe Gabriel de Lira Moura <filipe@direction.com.br>
2024-11-01 16:21:34 -0300

    Ajustment in real currency pipe

--- src/app/pipes/real-currency.pipe.ts
+++ src/app/pipes/real-currency.pipe.ts
@@ -6,11 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RealCurrencyPipe implements PipeTransform {
 
-  transform(value: number | null): string {
-    if (value === null || value === undefined) {
-        return '';
-      }
-      return `R$ ${value.toFixed(2).replace('.', ',')}`;
-  }
+    transform(value: number | null): string {
+        if (value === null || value === undefined) {
+          return '';
+        }
+
+        const formattedValue = value.toFixed(2).replace('.', ',');
+
+        const [integerPart, decimalPart] = formattedValue.split(',');
+
+        const integerWithDots = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+        return `R$ ${integerWithDots},${decimalPart}`;
+    }
 
 }
