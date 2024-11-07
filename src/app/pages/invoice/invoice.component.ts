394d5f6714ad2b3aec9adf4f6c66d03ffa0e2fd1
Filipe Gabriel de Lira Moura <filipe@direction.com.br>
2024-11-07 11:46:09 -0300

    ExpensesItem json mockup, expenses item service created

--- src/app/pages/invoice/invoice.component.ts
+++ src/app/pages/invoice/invoice.component.ts
@@ -10,6 +10,7 @@ import { RealCurrencyPipe } from '../../pipes/real-currency.pipe';
 import { InvoiceService } from '../../services/invoice.service';
 import { SpentTypeService } from '../../services/spent-type.service';
 import { ToastrService } from 'ngx-toastr';
+import { ExpensesItemService } from '../../services/expenses-item.service';
 
 @Component({
   selector: 'app-invoice',
@@ -23,8 +24,7 @@ export class InvoiceComponent implements OnInit {
     invoices: Invoice[];
     spentTypes: SpentType[];
     totalValueSpent: number;
-    item: ExpensesItem;
-    items: ExpensesItem[];
+    expensesItems: ExpensesItem[];
     totalValueExpenses: number;
     monthlyContract: MonthlyContract;
     monthlyContracts: MonthlyContract[];
@@ -37,14 +37,15 @@ export class InvoiceComponent implements OnInit {
     isExpenseCollapsed = false;
     isContractsCollapsed = false;
 
-    spentItems = ['', 'Santander', 'Inter', 'NuBank'];
-    selectedTypeExpenses: string = this.spentItems[0];
+    spentItems: string[];
+    selectedTypeExpenses: string;
 
     constructor(
         private toastr: ToastrService,
         private monthTranslate: MonthTranslateService,
         private invoiceService: InvoiceService ,
-        private spentTypeService: SpentTypeService
+        private spentTypeService: SpentTypeService,
+        private expensesItemService: ExpensesItemService
     ) { }
 
     ngOnInit() {
@@ -56,7 +57,7 @@ export class InvoiceComponent implements OnInit {
                 ...invoice,
                 monthName: this.monthTranslate.translate(invoice.monthName)
                 }));
-            });
+        });
 
         this.spentTypeService
             .getSpentTypes()
@@ -64,46 +65,24 @@ export class InvoiceComponent implements OnInit {
                 next: (response) => {
                 this.spentTypes = response;
                 this.calculateTotalSpent();
+                this.getSpentItems();
                 },
                 error: (error) => {
                     this.toastr.error(error.error.error, 'Erro ao cadastrar!');
                 }
-            });
+        });
 
-        this.items = [
-            this.item = {
-                description: 'Televisão',
-                installment: 4,
-                itemValue: 150,
-                spentTypeId: 1,
-                monthlyExpensesId: 1
-            },
-            this.item = {
-                description: 'Pratos',
-                installment: 2,
-                itemValue: 50,
-                spentTypeId: 1,
-                monthlyExpensesId: 1
-            },this.item = {
-                description: 'Geladeira',
-                installment: 10,
-                itemValue: 260,
-                spentTypeId: 1,
-                monthlyExpensesId: 1
-            },
-            this.item = {
-                description: 'Mesa',
-                installment: 7,
-                itemValue: 80,
-                spentTypeId: 1,
-                monthlyExpensesId: 1
-            }
-        ]
-
-        this.totalValueExpenses = 0
-        for (let item of this.items) {
-            this.totalValueExpenses += item.itemValue;
-        }
+        this.expensesItemService
+            .getExpensesItem()
+            .subscribe({
+                next: (response) => {
+                this.expensesItems = response;
+                this.calculateTotalExpensesItems();
+                },
+                error: (error) => {
+                    this.toastr.error(error.error.error, 'Erro ao cadastrar!');
+                }
+        });
 
         this.monthlyContracts = [
             this.monthlyContract = {
@@ -176,6 +155,15 @@ export class InvoiceComponent implements OnInit {
 
     calculateTotalSpent(): void {
         this.totalValueSpent = this.spentTypes.reduce((sum, SpentType) => sum + SpentType.spentValue, 0);
+    }
+
+    getSpentItems(): void {
+        this.spentItems = [''].concat(this.spentTypes.map(spentType => spentType.description));
+        this.selectedTypeExpenses = this.spentItems[0];
+    }
+
+    calculateTotalExpensesItems(): void {
+        this.totalValueExpenses = this.expensesItems.reduce((sum, expensesItems) => sum + expensesItems.itemValue, 0);
     }
 
 }
---
+++ src/app/services/expenses-item.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExpensesItem } from '../models/expenses-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpensesItemService {
+
+    apiUrlBase: string = 'assets/data/expenses-item.json';
+
+    constructor(private http: HttpClient) {}
+
+    getExpensesItem(): Observable<ExpensesItem[]> {
+      return this.http.get<ExpensesItem[]>(this.apiUrlBase);
+    }
+
+}
