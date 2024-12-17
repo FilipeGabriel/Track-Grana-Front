7f3b50a117bb970e9e5b14adaf5720b03a652aca
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-12-16 22:50:32 -0300

    Adjustment in the interceptor to not send the bearer token on public routes

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -3,9 +3,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
     const authToken = localStorage.getItem('access_token');
 
-    if (authToken) {
+    const publicRoutes = ['/auth/register', '/auth/login'];
+
+    const isPublicRoute = publicRoutes.some(route => req.url.includes(route));
+
+    if (authToken && !isPublicRoute) {
         const authReq = req.clone({
-            headers: req.headers.set('Authorization', `Bearer ${authToken}`), // Use o token diretamente
+            headers: req.headers.set('Authorization', `Bearer ${authToken}`),
         });
         return next(authReq);
     }
